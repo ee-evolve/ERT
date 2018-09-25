889ffd87591694d7053e4945ab2cec91b6294c48
Cristian Gavrila <cgavrila@equalexperts.com>
2018-09-25 16:18:22 +0100

    Send message to slack when new version is out

--- tests/test_updater.py
+++ tests/test_updater.py
@@ -1,10 +1,10 @@
 import unittest
 import updater
-from datetime import datetime, timedelta
+import datetime
 from mock import *
 
-mock_datetime = Mock()
-mock_datetime.return_value = datetime(2018, 5, 10, 10, 0, 0)
+# mock_datetime = Mock()
+# mock_datetime.return_value = datetime(2018, 5, 10, 10, 0, 0)
 
 class UpdaterTest(unittest.TestCase):
 
@@ -23,9 +23,21 @@ class UpdaterTest(unittest.TestCase):
         url = updater.create_url('test/demo')
         self.assertEqual(url, 'https://github.com/test/demo/releases.atom')
 
-    @patch('datetime.now()', mock_datetime)
+    @patch.object(updater, 'datetime', Mock(wraps=datetime.datetime))
     def test_release_is_25h_old(self):
-        self.assertFalse(updater.is_datetime_in_last_24h(datetime(2018, 5, 9, 9, 0, 0)))
+        updater.datetime.now.return_value = datetime.datetime(2018, 5, 10, 10, 0, 0)
+        self.assertFalse(updater.is_datetime_in_last_24h(datetime.datetime(2018, 5, 9, 9, 0, 0)))
+
+    @patch.object(updater, 'datetime', Mock(wraps=datetime.datetime))
+    def test_release_is_23h_old(self):
+        updater.datetime.now.return_value = datetime.datetime(2018, 5, 10, 10, 0, 0)
+        self.assertTrue(updater.is_datetime_in_last_24h(datetime.datetime(2018, 5, 9, 11, 0, 0)))
+
+    @patch.object(updater, 'datetime', Mock(wraps=datetime.datetime))
+    def test_release_is_24h_old(self):
+        updater.datetime.now.return_value = datetime.datetime(2018, 5, 10, 10, 0, 0)
+        self.assertTrue(updater.is_datetime_in_last_24h(datetime.datetime(2018, 5, 9, 10, 0, 0)))
+
 
 if __name__ == '__main__':
     unittest.main()
--- updater.py
+++ updater.py
@@ -1,8 +1,10 @@
 import atoma
 import requests
-from datetime import datetime, timedelta
+import slack
+from datetime import datetime, timedelta, timezone
 
 REPOS_FILENAME = 'repos.txt'
+
 
 def get_repos_list(filename):
     if not filename:
@@ -23,20 +25,29 @@ def get_atom_feed(url):
 
 
 def get_entries_from_parsed_atom_feed(parsed_atom_feed):
-    return parsed_atom_feed['entries']
+    return parsed_atom_feed.entries
 
 
 def is_datetime_in_last_24h(release_datetime):
-    return datetime.now() - timedelta(hours=24) <= release_datetime
+    return datetime.now(timezone.utc) - timedelta(hours=100) <= release_datetime
 
+
+def retrieve_releases_in_last_24h(parsed_atom_feeds):
+    entries = list(map(get_entries_from_parsed_atom_feed, parsed_atom_feeds))
+    return filter(lambda x: is_datetime_in_last_24h(x.updated), entries[0])
 
 def main():
     repos_list = get_repos_list(REPOS_FILENAME)
-    urls = list(map(create_url, repos_list))
-    atom_feeds = list(map(get_atom_feed, urls))
+    urls = map(create_url, repos_list)
+    atom_feeds = map(get_atom_feed, urls)
     parsed_atom_feeds = map(lambda x: atoma.parse_atom_bytes(x), atom_feeds)
+    releases_last_24h = list(retrieve_releases_in_last_24h(parsed_atom_feeds))
 
-    print(list(parsed_atom_feeds))
+    versions_list = list(map(lambda x: x.title.value, releases_last_24h))
+
+    print(versions_list)
+
+    slack.send_slack_message('New versions released: ' + ', '.join(versions_list))
 
 if __name__ == '__main__':
     main()
