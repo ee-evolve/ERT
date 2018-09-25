affaeb6ad8be141f279c2e4135ed17a1556cea41
Harvi Khaira <hkhaira@equalexperts.com>
2018-09-25 12:00:02 +0100

    Added file loading and url generation methods (and tests)

--- tests/updater_test.py
+++ tests/updater_test.py
@@ -1,10 +1,23 @@
 import unittest
+import updater
 
 
-class Test(unittest.TestCase):
+class UpdaterTest(unittest.TestCase):
 
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+    EMPTY_REPOS_LIST_FILENAME = 'resources/empty_repos_list.txt'
+    REPOS_LIST_FILENAME = 'resources/repos_list.txt'
+
+    def test_empty_file_parsing(self):
+        repos_list = updater.get_repos_list(self.EMPTY_REPOS_LIST_FILENAME)
+        self.assertListEqual(repos_list, [])
+
+    def test_file_parsing(self):
+        repos_list = updater.get_repos_list(self.REPOS_LIST_FILENAME)
+        self.assertListEqual(repos_list, ['containous/traefik'])
+
+    def test_url_generation(self):
+        url = updater.create_url('test/demo')
+        self.assertEqual(url, 'https://github.com/test/demo/releases.atom')
 
 
 if __name__ == '__main__':
--- updater.py
+++ updater.py
@@ -1,6 +1,26 @@
 import atoma
 import requests
 
-response = requests.get('http://lucumr.pocoo.org/feed.atom')
-feed = atoma.parse_atom_bytes(response.content)
-print(feed)
+
+def get_repos_list(filename):
+    if not filename:
+        raise Exception("Invalid filename")
+
+    with open(filename) as f:
+        content = f.readlines()
+    content = [x.strip() for x in content]
+    return content
+
+
+def create_url(repo_name):
+    return 'https://github.com/' + repo_name + '/releases.atom'
+
+
+def main():
+    response = requests.get('http://lucumr.pocoo.org/feed.atom')
+    feed = atoma.parse_atom_bytes(response.content)
+    print(feed)
+
+
+if __name__ == '__main__':
+    main()
